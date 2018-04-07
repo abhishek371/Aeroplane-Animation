9cff54b4ee8146f4ddc0a524fef853fe29660624
abhishek371 <kamalabhishek20@gmail.com>
2018-04-08 02:08:33 +0530

    Files added

--- Aeroplane_Animator/main.cpp
+++ Aeroplane_Animator/main.cpp
@@ -2,6 +2,12 @@
 #include<GL/glut.h>
 #include<stdio.h>
 GLfloat x1,y1,x2,y2,x3,y3,x4,y4;
+GLfloat div_x1,div_y1,div_x2,div_y2,div_x3,div_y3,div_x4,div_y4;
+
+
+
+int SCREEN_WIDTH=1920;
+int SCREEN_HEIGHT=1080;
 
 void edgedetect(GLfloat x1,GLfloat y1,GLfloat x2,GLfloat y2,int *le,int *re)
 {
@@ -27,47 +33,43 @@ void edgedetect(GLfloat x1,GLfloat y1,GLfloat x2,GLfloat y2,int *le,int *re)
             }
 }
 
-void drawpixel(int x,int y)
+void drawpixel(int x,int y,float r, float g, float b)
 {
-            glColor3f(1.0,0.0,0.0);
+            glColor3f(r,g,b);
             glBegin(GL_POINTS);
             glVertex2i(x,y);
             glEnd();
 }
 
-void scanfill(float x1,float y1,float x2,float y2,float x3,float y3,float x4,float y4)
+void scanfill(float x1,float y1,float x2,float y2,float x3,float y3,float x4,float y4,float r,float g,float b)
 {
-            int le[500],re[500];
+            int le[SCREEN_WIDTH],re[SCREEN_HEIGHT];
             int i,y;
-            for(i=0;i<500;i++)
+            for(i=0;i<SCREEN_WIDTH;i++)
             {
-                        le[i]=500;
+                        le[i]=SCREEN_WIDTH;
                         re[i]=0;
             }
             edgedetect(x1,y1,x2,y2,le,re);
             edgedetect(x2,y2,x3,y3,le,re);
             edgedetect(x3,y3,x4,y4,le,re);
             edgedetect(x4,y4,x1,y1,le,re);
-for(y=0;y<500;y++)
+for(y=0;y<SCREEN_HEIGHT;y++)
 {
            if(le[y]<=re[y])
                         for(i=le[y]+1;i<re[y];i++)
-                                    drawpixel(i,y);
+                                    drawpixel(i,y,r,g,b);
 }
 }
 
 void runaway(void)
 {
-    x1=0,y1=0,x2=0,y2=500,x3=1920,y3=500,x4=1920,y4=0;
+    x1=0,y1=50,x2=0,y2=350,x3=1920,y3=350,x4=1920,y4=50;
+    scanfill(x1,y1,x2,y2,x3,y3,x4,y4,0.000,0.000,0.000);
+    div_x1=25,div_x2=25,div_x3=75,div_x4=75;
+    div_y1=160,div_y2=190,div_y3=190,div_y4=160;
+    scanfill(div_x1,div_y1,div_x2,div_y2,div_x3,div_y3,div_x4,div_y4,0.753,0.753,0.753);
 
-    glBegin(GL_LINE_LOOP);
-    glVertex2f(x1,y1);
-    glVertex2f(x2,y2);
-    glVertex2f(x3,y3);
-    glVertex2f(x4,y4);
-    glEnd();
-
-    scanfill(x1,y1,x2,y2,x3,y3,x4,y4);
     glFlush();
 
 
@@ -77,19 +79,13 @@ void display(void)
 {
 glClear(GL_COLOR_BUFFER_BIT);
 runaway();
-/*glPointSize(10.0);
-glBegin(GL_POINTS);
-glColor3f(1.0,1.0,1.0);
-glVertex2d(10,10);
-glVertex2d(20,50);
-glEnd();*/
 glutSwapBuffers();
 }
 
 
 void myInit(void)
 {
-	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glClearColor(0.878,1.000,1.000,1.0);
 	gluOrtho2D(0.0,1920.0,0.0,1080.0);
 }
 
@@ -97,7 +93,7 @@ int main(int argc,char *argv[])
 {
     glutInit(&argc,argv);
     glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA|GLUT_DEPTH);
-    glutInitWindowSize(1920,1080);
+    glutInitWindowSize(SCREEN_WIDTH,SCREEN_HEIGHT);
     glutInitWindowPosition(0,0);
     glutCreateWindow("Frame_1");
     myInit();
